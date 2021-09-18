51cde8bd29c834aef11488c4fdf157353428c4d3
Jefferson Frigo <jrfrigo1@gmail.com>
2021-09-18 15:40:49 -0300

    Adicionando Classes Base
    As classes base da proposta foram adicionadas e seu código interno foi copiado.

---
+++ Atividade2LeituraJSON/Atividade2LeituraJSON/Atributos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade2LeituraJSON
+{
+	class Atributos
+	{
+		public int ClasseId { get; set; }
+		public int Forca { get; set; }
+		public int Destreza { get; set; }
+		public int Inteligencia { get; set; }
+	}
+}
---
+++ Atividade2LeituraJSON/Atividade2LeituraJSON/Classe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade2LeituraJSON
+{
+	class Classe
+	{
+		public int Id { get; set; }
+		public string NomeClasse { get; set; }
+		public Atributos Atributos { get; set; }
+	}
+}

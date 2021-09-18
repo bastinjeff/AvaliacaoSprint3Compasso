8cdbb3eb012535289c47b2a9096d21cf858e9c5b
Jefferson Frigo <jrfrigo1@gmail.com>
2021-09-18 16:13:10 -0300

    Classes Base adicionadas
    Adicionando classes base para o projeto

---
+++ Atividade2LeituraJSON/Atividade2LeituraJSON/IdsRoot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade2LeituraJSON
+{
+	class IdsRoot
+	{
+		public IEnumerable<int> Ids { get; set; }
+	}
+}
